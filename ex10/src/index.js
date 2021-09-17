c3a1e03b6a75cd694708e0ce62f9152ea293156f
Narda16 <nardabrodovic@gmail.com>
2021-09-17 09:58:40 +0200

    Repo04 done

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+  var myFirstName = "Narda";
+  var myLastName = "Brodovic";
+  var myHomeTown = "Sarajevo";
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+
+module.exports = main;
